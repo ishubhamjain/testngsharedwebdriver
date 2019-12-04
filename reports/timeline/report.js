95570c178ef45ae62afa32df87de7cf1eef44503
ishubhamjain <ishubhamjain@gmail.com>
2019-12-04 18:32:43 +0530

    trying to run project from Main method

--- reports/timeline/report.js
+++ reports/timeline/report.js
@@ -4,93 +4,85 @@ CucumberHTML.timelineItems.pushArray([
     "id": "automation-google-feature-file-name;selenium",
     "feature": "Automation google feature file name",
     "scenario": "Selenium",
-    "start": 1575463358702,
-    "end": 1575463388503,
-    "group": 17,
+    "start": 1575464513179,
+    "end": 1575464513179,
+    "group": 14,
     "content": "",
-    "className": "passed",
+    "className": "undefined",
     "tags": ""
   },
   {
     "id": "automation-eclipse-feature-file-name;eclipse",
     "feature": "Automation eclipse feature file name",
     "scenario": "Eclipse",
-    "start": 1575463358702,
-    "end": 1575463392674,
-    "group": 18,
+    "start": 1575464513179,
+    "end": 1575464513179,
+    "group": 11,
     "content": "",
-    "className": "passed",
+    "className": "undefined",
     "tags": ""
   },
   {
     "id": "automation-goibobo-feature-file-name;postive-scenario",
     "feature": "Automation goibobo feature file name",
     "scenario": "postive scenario",
-    "start": 1575463358703,
-    "end": 1575463382954,
-    "group": 13,
+    "start": 1575464513173,
+    "end": 1575464513177,
+    "group": 11,
     "content": "",
-    "className": "passed",
-    "tags": ""
-  },
-  {
-    "id": "automation-google-feature-file-name;cucumber",
-    "feature": "Automation google feature file name",
-    "scenario": "Cucumber",
-    "start": 1575463358703,
-    "end": 1575463394128,
-    "group": 16,
-    "content": "",
-    "className": "passed",
+    "className": "undefined",
     "tags": ""
   },
   {
     "id": "automation-goibobo-feature-file-name;negative-scenario---source-and-destination-is-same",
     "feature": "Automation goibobo feature file name",
     "scenario": "Negative scenario - Source and Destination is same",
-    "start": 1575463358702,
-    "end": 1575463384538,
+    "start": 1575464513173,
+    "end": 1575464513177,
+    "group": 12,
+    "content": "",
+    "className": "undefined",
+    "tags": ""
+  },
+  {
+    "id": "automation-google-feature-file-name;cucumber",
+    "feature": "Automation google feature file name",
+    "scenario": "Cucumber",
+    "start": 1575464513173,
+    "end": 1575464513177,
     "group": 14,
     "content": "",
-    "className": "passed",
+    "className": "undefined",
     "tags": ""
   },
   {
     "id": "automation-goibobo-feature-file-name;negative-scenario---select-an-old-date-for-flight-booking",
     "feature": "Automation goibobo feature file name",
     "scenario": "Negative scenario - Select an old date for flight booking",
-    "start": 1575463358703,
-    "end": 1575463384895,
-    "group": 15,
+    "start": 1575464513173,
+    "end": 1575464513177,
+    "group": 13,
     "content": "",
-    "className": "passed",
+    "className": "undefined",
     "tags": ""
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
+    "id": 11,
+    "content": "Thread[cucumber-runner-1-thread-1,5,main]"
+  },
+  {
+    "id": 12,
+    "content": "Thread[cucumber-runner-1-thread-2,5,main]"
+  },
+  {
     "id": 13,
-    "content": "Thread[TestNG-PoolService-0,5,main]"
+    "content": "Thread[cucumber-runner-1-thread-3,5,main]"
   },
   {
     "id": 14,
-    "content": "Thread[TestNG-PoolService-1,5,main]"
-  },
-  {
-    "id": 15,
-    "content": "Thread[TestNG-PoolService-2,5,main]"
-  },
-  {
-    "id": 16,
-    "content": "Thread[TestNG-PoolService-3,5,main]"
-  },
-  {
-    "id": 17,
-    "content": "Thread[TestNG-PoolService-4,5,main]"
-  },
-  {
-    "id": 18,
-    "content": "Thread[TestNG-PoolService-5,5,main]"
+    "content": "Thread[cucumber-runner-1-thread-4,5,main]"
   }
 ]);
 });
--- src/test/java/parallel/RunnerTest.java
+++ src/test/java/parallel/RunnerTest.java
@@ -1,20 +1,36 @@
 package parallel;
 
 import io.cucumber.core.cli.Main;
+import io.cucumber.testng.CucumberOptions;
 
 //Source:
 
 //https://tutorial.grasshopper.tech/parallel-execution
 
+// TO-DO not working yet
+
+@CucumberOptions(plugin = {"timeline:reports/timeline"})
+
 public class RunnerTest {
 	
 	public static void main(String[] args) {
-		String[] params = new String[] { 
-				"--threads", "3", "-g", "parallel",
+		/*String[] params = new String[] { 
+				"--threads", "4", "-g", "parallel",
 				"-p", "timeline:reports/timeline", 
-				"src/main/resources/features"
+				"src/test/resources/features"
 				//,"src/main/java/stepDefinition"
 				};
-		Main.run(params, Thread.currentThread().getContextClassLoader());
+		Main.run(params, Thread.currentThread().getContextClassLoader());*/
+		
+		String [] argv =  new String[] { 
+				"--threads", "4", "-g", "parallel",
+				"-p", "timeline:reports/timeline", 
+				"./src/test/resources/features/"
+				//,"src/main/java/stepDefinition"
+				};
+		
+	//	String [] argv = new String[]{ "-g","","./src/test/resources/features/"};
+		ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+		byte exitstatus = Main.run(argv, contextClassLoader);
 	}
 }
