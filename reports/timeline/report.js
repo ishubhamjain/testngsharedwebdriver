d382b0d7b9da3dbde58575b9481b2220a975f072
ishubhamjain <ishubhamjain@gmail.com>
2019-12-04 22:20:21 +0530

    report not genrating error case

--- reports/timeline/report.js
+++ reports/timeline/report.js
@@ -1,34 +1,23 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "automation-google-feature-file-name;selenium",
-    "feature": "Automation google feature file name",
-    "scenario": "Selenium",
-    "start": 1575464903850,
-    "end": 1575464931573,
-    "group": 17,
-    "content": "",
-    "className": "passed",
-    "tags": ""
-  },
-  {
     "id": "automation-eclipse-feature-file-name;eclipse",
     "feature": "Automation eclipse feature file name",
     "scenario": "Eclipse",
-    "start": 1575464903850,
-    "end": 1575464923662,
-    "group": 18,
+    "start": 1575477815220,
+    "end": 1575477887809,
+    "group": 15,
     "content": "",
     "className": "passed",
     "tags": ""
   },
   {
-    "id": "automation-goibobo-feature-file-name;postive-scenario",
-    "feature": "Automation goibobo feature file name",
-    "scenario": "postive scenario",
-    "start": 1575464903850,
-    "end": 1575464926187,
-    "group": 13,
+    "id": "automation-google-feature-file-name;selenium",
+    "feature": "Automation google feature file name",
+    "scenario": "Selenium",
+    "start": 1575477815220,
+    "end": 1575477891335,
+    "group": 14,
     "content": "",
     "className": "passed",
     "tags": ""
@@ -37,31 +26,9 @@ CucumberHTML.timelineItems.pushArray([
     "id": "automation-google-feature-file-name;cucumber",
     "feature": "Automation google feature file name",
     "scenario": "Cucumber",
-    "start": 1575464903850,
-    "end": 1575464926435,
-    "group": 16,
-    "content": "",
-    "className": "passed",
-    "tags": ""
-  },
-  {
-    "id": "automation-goibobo-feature-file-name;negative-scenario---source-and-destination-is-same",
-    "feature": "Automation goibobo feature file name",
-    "scenario": "Negative scenario - Source and Destination is same",
-    "start": 1575464903850,
-    "end": 1575464923427,
-    "group": 14,
-    "content": "",
-    "className": "passed",
-    "tags": ""
-  },
-  {
-    "id": "automation-goibobo-feature-file-name;negative-scenario---select-an-old-date-for-flight-booking",
-    "feature": "Automation goibobo feature file name",
-    "scenario": "Negative scenario - Select an old date for flight booking",
-    "start": 1575464903852,
-    "end": 1575464928869,
-    "group": 15,
+    "start": 1575477815224,
+    "end": 1575477902448,
+    "group": 13,
     "content": "",
     "className": "passed",
     "tags": ""
@@ -79,18 +46,6 @@ CucumberHTML.timelineGroups.pushArray([
   {
     "id": 15,
     "content": "Thread[TestNG-PoolService-2,5,main]"
-  },
-  {
-    "id": 16,
-    "content": "Thread[TestNG-PoolService-3,5,main]"
-  },
-  {
-    "id": 17,
-    "content": "Thread[TestNG-PoolService-4,5,main]"
-  },
-  {
-    "id": 18,
-    "content": "Thread[TestNG-PoolService-5,5,main]"
   }
 ]);
 });
--- src/test/resources/features/A.MyFeatures.feature
+++ src/test/resources/features/A.MyFeatures.feature
@@ -22,20 +22,20 @@ Feature: Automation goibobo feature file name
   Background:
   Given User logs into the application and on the home page
   
-  Scenario: postive scenario
-  Given verify if GoIbibo homepage goibibo works as expected
-  When search for one-way flights between "Bengaluru" and "Mumbai"
-  Then verify if the page results are ordered in decreasing order of cost
-  
-  Scenario: Negative scenario - Source and Destination is same
-  Given verify if GoIbibo homepage goibibo works as expected
-  When search for one-way flights between "Bengaluru" and "Bengaluru"
-  Then An error message should appears and no search process should initiate
-  
-  Scenario: Negative scenario - Select an old date for flight booking
-  Given verify if GoIbibo homepage goibibo works as expected
-  When search for one-way flights between "Bengaluru" and "Mumbai" on old date
-  Then Old date must not be selected
+  #Scenario: postive scenario
+  #Given verify if GoIbibo homepage goibibo works as expected
+  #When search for one-way flights between "Bengaluru" and "Mumbai"
+  #Then verify if the page results are ordered in decreasing order of cost
+  #
+  #Scenario: Negative scenario - Source and Destination is same
+  #Given verify if GoIbibo homepage goibibo works as expected
+  #When search for one-way flights between "Bengaluru" and "Bengaluru"
+  #Then An error message should appears and no search process should initiate
+  #
+  #Scenario: Negative scenario - Select an old date for flight booking
+  #Given verify if GoIbibo homepage goibibo works as expected
+  #When search for one-way flights between "Bengaluru" and "Mumbai" on old date
+  #Then Old date must not be selected
   
 
   
