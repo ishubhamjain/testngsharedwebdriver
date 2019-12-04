96cbdd7bf7ddffe6dd5dd840edc4ee7f622f4ec3
ishubhamjain <ishubhamjain@gmail.com>
2019-12-04 18:39:39 +0530

    added descriptioin with url

--- reports/timeline/report.js
+++ reports/timeline/report.js
@@ -4,85 +4,93 @@ CucumberHTML.timelineItems.pushArray([
     "id": "automation-google-feature-file-name;selenium",
     "feature": "Automation google feature file name",
     "scenario": "Selenium",
-    "start": 1575464513179,
-    "end": 1575464513179,
-    "group": 14,
+    "start": 1575464903850,
+    "end": 1575464931573,
+    "group": 17,
     "content": "",
-    "className": "undefined",
+    "className": "passed",
     "tags": ""
   },
   {
     "id": "automation-eclipse-feature-file-name;eclipse",
     "feature": "Automation eclipse feature file name",
     "scenario": "Eclipse",
-    "start": 1575464513179,
-    "end": 1575464513179,
-    "group": 11,
+    "start": 1575464903850,
+    "end": 1575464923662,
+    "group": 18,
     "content": "",
-    "className": "undefined",
+    "className": "passed",
     "tags": ""
   },
   {
     "id": "automation-goibobo-feature-file-name;postive-scenario",
     "feature": "Automation goibobo feature file name",
     "scenario": "postive scenario",
-    "start": 1575464513173,
-    "end": 1575464513177,
-    "group": 11,
+    "start": 1575464903850,
+    "end": 1575464926187,
+    "group": 13,
     "content": "",
-    "className": "undefined",
-    "tags": ""
-  },
-  {
-    "id": "automation-goibobo-feature-file-name;negative-scenario---source-and-destination-is-same",
-    "feature": "Automation goibobo feature file name",
-    "scenario": "Negative scenario - Source and Destination is same",
-    "start": 1575464513173,
-    "end": 1575464513177,
-    "group": 12,
-    "content": "",
-    "className": "undefined",
+    "className": "passed",
     "tags": ""
   },
   {
     "id": "automation-google-feature-file-name;cucumber",
     "feature": "Automation google feature file name",
     "scenario": "Cucumber",
-    "start": 1575464513173,
-    "end": 1575464513177,
+    "start": 1575464903850,
+    "end": 1575464926435,
+    "group": 16,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "automation-goibobo-feature-file-name;negative-scenario---source-and-destination-is-same",
+    "feature": "Automation goibobo feature file name",
+    "scenario": "Negative scenario - Source and Destination is same",
+    "start": 1575464903850,
+    "end": 1575464923427,
     "group": 14,
     "content": "",
-    "className": "undefined",
+    "className": "passed",
     "tags": ""
   },
   {
     "id": "automation-goibobo-feature-file-name;negative-scenario---select-an-old-date-for-flight-booking",
     "feature": "Automation goibobo feature file name",
     "scenario": "Negative scenario - Select an old date for flight booking",
-    "start": 1575464513173,
-    "end": 1575464513177,
-    "group": 13,
+    "start": 1575464903852,
+    "end": 1575464928869,
+    "group": 15,
     "content": "",
-    "className": "undefined",
+    "className": "passed",
     "tags": ""
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
-    "id": 11,
-    "content": "Thread[cucumber-runner-1-thread-1,5,main]"
-  },
-  {
-    "id": 12,
-    "content": "Thread[cucumber-runner-1-thread-2,5,main]"
-  },
-  {
     "id": 13,
-    "content": "Thread[cucumber-runner-1-thread-3,5,main]"
+    "content": "Thread[TestNG-PoolService-0,5,main]"
   },
   {
     "id": 14,
-    "content": "Thread[cucumber-runner-1-thread-4,5,main]"
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 15,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  },
+  {
+    "id": 16,
+    "content": "Thread[TestNG-PoolService-3,5,main]"
+  },
+  {
+    "id": 17,
+    "content": "Thread[TestNG-PoolService-4,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[TestNG-PoolService-5,5,main]"
   }
 ]);
 });
